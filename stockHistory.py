d481d3934851a156dbe05f2bd9821722ca4087c4
FogadOg <fogadm06@gmail.com>
2024-03-09 18:48:52 +0100

    module to get a stocks history for a timeframe

---
+++ stockHistory.py
@@ -0,0 +1,35 @@
+import yfinance as yf
+import datetime
+import mplfinance as mpf
+from stringToDatetime import StringToDatetime
+from stockSymbol import companySymbols
+
+class StockHistory():
+    def __init__(self, companyName, articalPublishTimeString, timeFrameInHours = 1):
+        self.articalPublishTimeString = articalPublishTimeString
+        self.timeFrameInHours = timeFrameInHours
+
+        tickerSymbol = companySymbols[companyName]
+
+        self.data = yf.download(tickerSymbol, period='1d', interval='1m')
+
+        self.data.index = self.data.index.tz_convert('America/New_York')
+
+        self.data['Datetime'] = self.data.index
+
+        self.stockDataForTimeframe = self.getStockDataForTimeframe()
+
+    def getStockDataForTimeframe(self):
+        articalPublishTime = StringToDatetime(self.articalPublishTimeString).getDatetime()
+
+        startTime = articalPublishTime.replace(tzinfo=datetime.timezone(datetime.timedelta(hours=-5)))
+        endTime = startTime + datetime.timedelta(hours=self.timeFrameInHours)
+        return self.data[(self.data['Datetime'] >= startTime) & (self.data['Datetime'] <= endTime)]
+
+
+    def renderChart(self):
+        mpf.plot(self.stockDataForTimeframe, type='candle', style='charles', volume=True)
+
+
+if __name__ == "__main__":
+    StockHistory('Apple', "FRI, MAR 8 202410:00 AM EST").renderChart()
