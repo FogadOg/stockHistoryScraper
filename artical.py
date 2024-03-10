5bee9a7c4a483ced9660629a23bffae43a641404
FogadOg <fogadm06@gmail.com>
2024-03-10 12:28:53 +0100

    extracting title from artical

--- artical.py
+++ artical.py
@@ -1,7 +1,24 @@
+import requests, spacy
+from bs4 import BeautifulSoup
+nlp = spacy.load("en_core_web_sm")
+
 class Artical():
     def __init__(self, url):
-        pass
+        self.url = url
+        response = requests.get(url)
+        self.soup = BeautifulSoup(response.text, "html.parser")
+        
+        self.title = self.getTitle()
+
+    
+    def getTitle(self) -> str:
+        titleElement = self.soup.find(class_="ArticleHeader-headline")
+        return titleElement.text
+
 
 
     
+    def __str__(self):
+        return self.url
+
 
