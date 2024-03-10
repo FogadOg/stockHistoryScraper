09bd81fa019271cfaa9014f0efbbe3fad9ef2dad
FogadOg <fogadm06@gmail.com>
2024-03-10 18:06:14 +0100

    raising error if user tries to get data for after closing hours

--- stock/stockHistory.py
+++ stock/stockHistory.py
@@ -4,8 +4,8 @@ import mplfinance as mpf
 from stockSymbol import stockSymbole
 
 class StockHistory():
-    def __init__(self, companyName: str, articalPublishTime: datetime, timeFrameInHours = 1):
-        self.articalPublishTime = articalPublishTime
+    def __init__(self, companyName: str, articalPublishTime: datetime.datetime, timeFrameInHours = 1):
+        self.articalPublishTime = articalPublishTime.replace(tzinfo=datetime.timezone(datetime.timedelta(hours=-5)))
         self.timeFrameInHours = timeFrameInHours
 
         tickerSymbol = self.getCompanysTikcer(companyName)
@@ -27,13 +27,15 @@ class StockHistory():
 
 
     def getStockDataForTimeframe(self):
-        startTime = self.articalPublishTime.replace(tzinfo=datetime.timezone(datetime.timedelta(hours=-5)))
-        endTime = startTime + datetime.timedelta(hours=self.timeFrameInHours)
-        return self.data[(self.data['Datetime'] >= startTime) & (self.data['Datetime'] <= endTime)]
+        if self.articalPublishTime.hour<15:
+            endTime = self.articalPublishTime + datetime.timedelta(hours=self.timeFrameInHours)
+            return self.data[(self.data['Datetime'] >= self.articalPublishTime) & (self.data['Datetime'] <= endTime)]
+        
+        raise IndexError(f"youre trying to get date for after closing times. your time is {self.articalPublishTime}")
 
     def renderChart(self):
         mpf.plot(self.stockDataForTimeframe, type='candle', style='charles', volume=True)
 
 
 if __name__ == "__main__":
-    StockHistory('Apple', datetime.datetime(2024, 3, 8, 10, 0, 0)).renderChart()
+    StockHistory('Apple', datetime.datetime(2024, 3, 8, 14, 30, 0)).renderChart()
