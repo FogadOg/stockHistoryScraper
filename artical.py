e42a51211f0ec3b0130038e5f98285d20b79b9fa
FogadOg <fogadm06@gmail.com>
2024-03-10 12:30:16 +0100

    changing the __str__ method

--- artical.py
+++ artical.py
@@ -4,7 +4,6 @@ nlp = spacy.load("en_core_web_sm")
 
 class Artical():
     def __init__(self, url):
-        self.url = url
         response = requests.get(url)
         self.soup = BeautifulSoup(response.text, "html.parser")
         
@@ -32,6 +31,6 @@ class Artical():
         return list(set(companies))
     
     def __str__(self):
-        return self.url
+        return self.title
 
 
