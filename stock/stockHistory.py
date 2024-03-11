3ff400d553f8a0c8c96f18c8ec5269535629e61a
FogadOg <115077292+FogadOg@users.noreply.github.com>
2024-03-11 10:08:33 +0100

    fixing import problem

--- stock/stockHistory.py
+++ stock/stockHistory.py
@@ -1,7 +1,7 @@
 import yfinance as yf
 import datetime
 import mplfinance as mpf
-from stockSymbol import stockSymbole
+from .stockSymbol import stockSymbole
 
 class StockHistory():
     def __init__(self, companyName: str, articalPublishTime: datetime.datetime, timeFrameInHours = 1):
