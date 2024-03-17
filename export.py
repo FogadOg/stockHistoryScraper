f99e0c2a51fe2c14269ad6ed54d10f5b8189c9f9
FogadOg <fogadm06@gmail.com>
2024-03-17 12:35:00 +0100

    fixing import error

--- export.py
+++ export.py
@@ -1,12 +1,7 @@
 import sys
 import os
-
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-
-sys.path.append(parent_dir)
-
 import csv
-from artical.artical import Artical
+from artical import Artical
 
 class Export():
     def __init__(self, artical: Artical, fileName:str = "data"):
