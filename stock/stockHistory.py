8e9bc89a831f5f05e89d15e9ed9ed56bf23437ad
FogadOg <fogadm06@gmail.com>
2024-03-17 07:42:41 +0100

    fixing bug where stock data isnt extracted

--- stock/stockHistory.py
+++ stock/stockHistory.py
@@ -1,7 +1,7 @@
 import yfinance as yf
 import datetime
 import mplfinance as mpf
-from .stockSymbol import stockSymbole
+from stockSymbol import stockSymbole
 
 class StockHistory():
     def __init__(self, companyName: str, articalPublishTime: datetime.datetime, timeFrameInHours = 1):
@@ -9,13 +9,7 @@ class StockHistory():
         self.articalPublishTime = articalPublishTime.replace(tzinfo=datetime.timezone(datetime.timedelta(hours=-4)))
         self.timeFrameInHours = timeFrameInHours
 
-        tickerSymbol = self.getCompanysTicker()
-
-        self.data = yf.download(tickerSymbol, period='1d', interval='1m')
-
-        self.data.index = self.data.index.tz_convert('America/New_York')
-
-        self.data['Datetime'] = self.data.index
+        self.tickerSymbol = self.getCompanysTicker()
 
         self.stockDataForTimeframe = self.getStockDataForTimeframe()
 
@@ -30,19 +24,23 @@ class StockHistory():
         except KeyError:
             raise KeyError("company not found in dictonary")
 
-
     def getStockDataForTimeframe(self):
-        if self.articalPublishTime.hour<15:
-            endTime = self.articalPublishTime + datetime.timedelta(hours=self.timeFrameInHours)
-            return self.data[(self.data['Datetime'] >= self.articalPublishTime) & (self.data['Datetime'] <= endTime)]
-        
-        raise IndexError(f"youre trying to get date for after closing times. your time is {self.articalPublishTime}")
+        marketOpenHour = 9
+        marketCloseHour = 15
+
+
+        if marketOpenHour <= self.articalPublishTime.hour < marketCloseHour:
+            endTime = self.articalPublishTime  + datetime.timedelta(hours=self.timeFrameInHours)
+
+            data = yf.download(self.tickerSymbol, period='1d', interval='1m', start=self.articalPublishTime, end=endTime)
+            return data
+        raise IndexError(f"You're trying to get data for after market closing hours. Your time is {self.articalPublishTime}")
 
     def renderChart(self):
         mpf.plot(self.stockDataForTimeframe, type='candle', style='charles', volume=True, title=self.companyName)
 
 
 if __name__ == "__main__":
-    stockHistory = StockHistory('Apple', datetime.datetime(2024, 3, 11, 12, 00, 0))
-    print(stockHistory["Open"])
-    stockHistory.renderChart()
+    stockHistory = StockHistory('Apple', datetime.datetime(2024, 3, 11, 12, 30, 0))
+    print(stockHistory)
+
