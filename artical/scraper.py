55ba578dacba1b6df36a9baeb88934cb05a96408
FogadOg <fogadm06@gmail.com>
2024-03-17 16:51:51 +0100

    some code for import

--- artical/scraper.py
+++ artical/scraper.py
@@ -1,6 +1,12 @@
+import  sys, os
+
+currentDir = os.path.dirname(os.path.abspath(__file__))
+parentDir = os.path.abspath(os.path.join(currentDir, '..'))
+sys.path.append(parentDir)
+
 import requests
 from bs4 import BeautifulSoup
-from artical import Artical
+from artical.artical import Artical
 from utils.export import Export
 
 class Scraper():
@@ -15,29 +21,6 @@ class Scraper():
         articals = self.getAllArticals()
         hrefs = self.findArticalHref(articals)
         return self._createArticalObjects(hrefs)
-
-    def getAllArticals(self):
-        articals = []
-        parentElements = self.soup.find_all(class_="RiverPlus-riverPlusContainer")
-
-        for parentElement in parentElements:
-            for artical in parentElement.children:
-                if "RiverPlusBreaker-container" not in artical.get('class', []):
-                    articals.append(artical)
-        return articals
-        
-    def findArticalHref(self, articals):
-        hrefs = []
-
-        for artical in articals:
-            aElements = artical.select(".RiverHeadline-headline.RiverHeadline-hasThumbnail a")
-            
-            for element in aElements:
-                href = element.get("href")
-                if self._isHrefValid(href):
-                    hrefs.append(href)
-
-        return hrefs
     
     def _isHrefValid(self, href):
         return "https" in href
@@ -52,8 +35,3 @@ class Scraper():
                 pass
         return articalObjects
 
-
-
-
-if __name__ == "__main__":
-    scraper = Scraper("https://www.cnbc.com/world/?region=world")
