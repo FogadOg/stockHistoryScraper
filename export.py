1aa017f0848bf8a0b2a00f9abae51a696c1cc0c7
FogadOg <fogadm06@gmail.com>
2024-03-17 11:35:02 +0100

    new module to write to csv file

---
+++ export.py
@@ -0,0 +1,40 @@
+import sys
+import os
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+sys.path.append(parent_dir)
+
+import csv
+from artical.artical import Artical
+
+class Export():
+    def __init__(self, artical: Artical, fileName:str = "data"):
+        self.artical = artical
+        self.filePath = f"{fileName}.csv"
+        self.export()
+
+    def export(self, csvFile="stockData.csv"):
+        if self._doesFileExist(csvFile):
+            self._writeHeader(csvFile)
+
+        for company in self.artical.releventCompanies:
+            history = self.artical.getStockHistory(company)
+            if history != None:
+                with open(csvFile, "a", newline="") as file:
+                    csvWriter = csv.writer(file)
+                    csvWriter.writerow([history.tickerSymbol, self.artical.content, history["Open"], history["Close"]])
+    
+    def _doesFileExist(self, csvFile):
+        return not os.path.exists(csvFile)
+
+    def _writeHeader(self, csvFile):        
+        with open(csvFile, "w", newline="") as file:
+            csvWriter = csv.writer(file)
+            csvWriter.writerow(["company", "News Artical", "Open", "Close"])
+
+
+if __name__ == "__main__":
+    artical = Artical("https://www.cnbc.com/2024/03/07/stock-market-today-live-updates.html")
+    Export(artical)
+
