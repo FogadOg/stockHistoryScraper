e6450b286e4e1690c3992ecc9f80853bb15af870
FogadOg <fogadm06@gmail.com>
2024-03-23 18:12:57 +0100

    changing timeFrames default value

--- stock/stockHistory.py
+++ stock/stockHistory.py
@@ -5,7 +5,7 @@ from .stockSymbol import stockSymbole
 from .productCompanyTicker import productsTickers
 
 class StockHistory():
-    def __init__(self, companyName: str, articlePublishTime: datetime.datetime, timeFrameInHours = 1):
+    def __init__(self, companyName: str, articlePublishTime: datetime.datetime, timeFrameInHours = 5):
         self.companyName = companyName.lower()
         self.articlePublishTime = articlePublishTime.replace(tzinfo=datetime.timezone(datetime.timedelta(hours=-4)))
         self.timeFrameInHours = timeFrameInHours
