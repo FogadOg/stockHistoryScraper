8e1ea816d89e4681f46efeeafb947c46cfc94a43
FogadOg <fogadm06@gmail.com>
2024-05-18 17:29:46 +0200

    improving module

--- utils/timeZone.py
+++ utils/timeZone.py
@@ -3,21 +3,36 @@ from datetime import datetime
 
 
 class TimeZone():
-    def __init__(self, datetimeObject: datetime):
-        self.datetimeObject = datetimeObject
+    def __init__(self, datetimeObject: datetime | str, localTime: str = "Europe/Paris"):
+
+        if isinstance(datetimeObject, datetime):
+            self.datetimeObject = datetimeObject
+        else:
+            self.datetimeObject = datetime.strptime(datetimeObject, "%Y-%m-%dT%H:%M:%S")
+
+        self.datetimeObject = self.setLocalTime(localTime)
+
+    def setLocalTime(self, localTime):
+        try:
+            timezone = pytz.timezone(localTime)
+            convertedDatetime = self.datetimeObject = timezone.localize(self.datetimeObject)
+            return convertedDatetime
+        except:
+            return self.datetimeObject
     
-    def convert(self, timeZone: str) -> datetime:
+    def convert(self, timeZone: str = "GMT") -> datetime:
         targetTimezone = pytz.timezone(timeZone)
         convertedDatetime = self.datetimeObject.astimezone(targetTimezone)
 
         return convertedDatetime
 
+    def __repr__(self) -> str:
+        return self.datetimeObject.strftime("%Y-%m-%dT%H:%M:%S")
+    
+
 
 
 if __name__ == "__main__":
-    time = datetime(2024, 3, 31, 12, 0, 0)
-    convert = TimeZone(time).convert("EST")
+    time = datetime(2024, 3, 31, 17, 0, 0)
+    convert = TimeZone(time,"GMT").convert('Europe/Paris')
     print(convert)
-
-
-
