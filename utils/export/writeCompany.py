9060a0a6a368845c08fa8f1ef4a25ce8d1c23939
FogadOg <fogadm06@gmail.com>
2024-05-23 19:10:06 +0200

    renaming file

--- utils/export/writeCompany.py
+++ utils/export/writeCompany.py
@@ -3,7 +3,7 @@ import os
 import csv
 from scrapers.article import Article
 
-class Export():
+class WriteCompany():
     def __init__(self, article: Article, fileName:str = "data"):
         self.article = article
         self.filePath = f"{fileName}.csv"
@@ -17,12 +17,10 @@ class Export():
             history = self.article.getStockHistory(company)
             if history != None:
                 data = history["stockDataForTimeframe"]
-                print(data)
                 with open(csvFile, "a", newline="") as file:
-                    openData = '[' +','.join(map(str, data["Open"].values)) + ']'
                     closeData = '[' +','.join(map(str, data["Close"].values)) + ']'
                     csvWriter = csv.writer(file)
-                    csvWriter.writerow([history.companyName, self.article.content, openData, closeData])
+                    csvWriter.writerow([history.tickerSymbol, self.article.content, closeData])
     
     def _doesFileExist(self, csvFile):
         return not os.path.exists(csvFile)
@@ -30,10 +28,10 @@ class Export():
     def _writeHeader(self, csvFile):        
         with open(csvFile, "w", newline="") as file:
             csvWriter = csv.writer(file)
-            csvWriter.writerow(["company", "News Article", "Open", "Close"])
+            csvWriter.writerow(["company", "News Article", "Close"])
 
 
 if __name__ == "__main__":
     article = Article("https://www.cnbc.com/2024/03/07/stock-market-today-live-updates.html")
-    Export(article)
+    WriteCompany(article)
 
