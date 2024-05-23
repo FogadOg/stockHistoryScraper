3bb320dcd186887ef3213485e3ee8e3562ded8fd
FogadOg <fogadm06@gmail.com>
2024-05-23 20:42:31 +0200

    scraping more elements

--- scrapers/cnbc/cnbcScraper.py
+++ scrapers/cnbc/cnbcScraper.py
@@ -21,13 +21,31 @@ class CnbcScraper(Scraper):
             for article in parentElement.children:
                 if "RiverPlusBreaker-container" not in article.get('class', []):
                     articles.append(article)
+
+                    
+        parentElements = self.soup.find_all(class_="LatestNews-list")
+
+        for parentElement in parentElements:
+            for article in parentElement.children:
+                if "LatestNews-item" in article.get('class', []):
+                    articles.append(article)
+
         return articles
+    
         
     def findArticleHref(self, articles):
         hrefs = []
 
+        hrefs += self._extractHref(articles, ".RiverHeadline-headline.RiverHeadline-hasThumbnail a")
+        hrefs += self._extractHref(articles, ".LatestNews-headlineWrapper a")
+
+        return hrefs
+
+    def _extractHref(self, articles, hrefClass):
+        hrefs = []
+
         for article in articles:
-            aElements = article.select(".RiverHeadline-headline.RiverHeadline-hasThumbnail a")
+            aElements = article.select(hrefClass)
             
             for element in aElements:
                 href = element.get("href")
@@ -42,3 +60,6 @@ class CnbcScraper(Scraper):
 
 if __name__ == "__main__":
     scraper = CnbcScraper()
+    articels = scraper.getAllArticles()
+    for article in articels:
+        print(articels.title)
